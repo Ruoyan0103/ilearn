7bfbe8d17a97ddeba0a1ada8d235ecb35fc3ae48
Ruoyan0103 <ruoyan.jin@aalto.fi>
2025-06-04 17:48:36 +0300

    calculate manually done

--- ilearn/lammps/submit-tde.sh
+++ ilearn/lammps/submit-tde.sh
@@ -6,6 +6,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=2G
-#SBATCH --job-name=thermalize
+#SBATCH --job-name=tde
 
+#SBATCH --array=0-20  # Change this if you have a different count
 
+module load gcc openmpi fftw openblas eigen ffmpeg zstd
+FOLDER=$(sed -n "$((SLURM_ARRAY_TASK_ID + 1))p" folder_list.txt)
+
+cd "$FOLDER"
+srun lmp -in in.tde
